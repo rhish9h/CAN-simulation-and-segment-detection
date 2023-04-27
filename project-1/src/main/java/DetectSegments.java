2466b6317b08db29e8927f3968874a07ab98eaf6
Jackson <jbraunsc@asu.edu>
2023-04-26 17:39:09 -0700

    Created a new class for detecting segment changes in yaw rate values

---
+++ project-1/src/main/java/DetectSegments.java
@@ -0,0 +1,45 @@
+import java.util.Collection;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class DetectSegments {
+    Queue<Double> yawValues;
+    double currentTotal;
+    boolean currentlyStraight;
+
+    public DetectSegments(){
+        yawValues = new PriorityQueue<>(10);
+        currentTotal = 0;
+        currentlyStraight = true;
+    }
+
+    public void newValue(double value){
+        if(yawValues.size() == 10){
+            currentTotal = currentTotal - yawValues.peek();
+            yawValues.remove();
+            yawValues.add(value);
+            currentTotal = currentTotal + value;
+
+            if(currentlyStraight){
+                if((currentTotal / 10.0) > 3.0 || (currentTotal / 10.0) < -3.0){
+                    currentlyStraight = false;
+                    System.out.println("Straight segment has ended, curved segment begins");
+                    currentTotal = 0;
+                    yawValues.clear();
+                }
+            }
+            else{
+                if(-3.0 < (currentTotal / 10.0) && (currentTotal / 10.0) < 3.0){
+                    currentlyStraight = true;
+                    System.out.println("Curved segment has ended, straight segment begins");
+                    currentTotal = 0;
+                    yawValues.clear();
+                }
+            }
+        }
+        else{ 
+            yawValues.add(value);
+            currentTotal = currentTotal + value;
+        }
+    }
+}
